94969b1935afc248023c81ab62a3f8f791063c6d
Stanislav Shamji <probojnik@gmail.com>
2013-08-31 19:05:33 +0300

    1

---
+++ src/com/probojnik/AbstractTaskLoader/SampleActivity.java
@@ -0,0 +1,15 @@
+package com.probojnik.AbstractTaskLoader;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SampleActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
